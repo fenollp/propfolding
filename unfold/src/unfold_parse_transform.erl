fddb19ba5c56c639b7fe79bd316865510a23a7d2
Pierre Fenoll <pierrefenoll@gmail.com>
2016-10-24 23:10:28 -0700

    unfold: remove routines match & replace fold call

--- unfold/src/unfold_parse_transform.erl
+++ unfold/src/unfold_parse_transform.erl
@@ -41,36 +41,40 @@ clause(S0, Clause={clause,_, _Args, _Guards, Body}) ->
     case maps:get(routines_var, S, nil) of
         nil -> Clause;
         _ ->
-            io:format("  ~p\n~p\n\n", [S, Clause]),
-            Clause
+            io:format("  ~p\n>>>~p\n\n", [S, Clause]),
+            Clause1 = setelement(5, Clause, rm_match(element(5,Clause), S)),
+            Clause2 = setelement(5, Clause1, replace_call(element(5,Clause1), S)),
+            NewClause = Clause2,
+            io:format("<<<~p\n\n", [NewClause]),
+            NewClause
     end.
 
-is_containing_rountines({match,_, {var,_,Var}, RHS}
+is_containing_rountines(Match={match,_, {var,_,Var}, RHS}
                        ,S) ->
     case is_list_of_funs(RHS, []) of
         false -> S;
         Fs ->
-            io:format("fun ~p/~p --- ~p\n", [maps:get(fname,S), maps:get(farity,S), Var]),
             S#{routines_var => Var
               ,routines => Fs
+              ,match => Match
               }
     end;
-is_containing_rountines({call,Loc
-                        ,{remote,_, {atom,_,lists}, {atom,_,foldl}}
-                        ,[{'fun',_
-                          ,{clauses,[{clause,_,
-                                      [{var,_,F}, {var,_,Arg}],
-                                      [],
-                                      [{call,_, {var,_,F}, [{var,_,Arg}]}]}]}}
-                         ,Acc0
-                         ,{var,_,RoutinesVar}]}
+is_containing_rountines(Call={call,Loc
+                             ,{remote,_, {atom,_,lists}, {atom,_,foldl}}
+                             ,[{'fun',_
+                               ,{clauses,[{clause,_,
+                                           [{var,_,F}, {var,_,Arg}],
+                                           [],
+                                           [{call,_, {var,_,F}, [{var,_,Arg}]}]}]}}
+                              ,Acc0
+                              ,{var,_,RoutinesVar}]}
                        ,#{fname := FName
                          ,routines_var := RoutinesVar
                          }=S) ->
-    io:format("~s ::: ~p ~p ~p\n"
-             ,[FName, RoutinesVar, F, Arg]),
+    io:format("~s ::: ~p ~p ~p\n", [FName, RoutinesVar, F, Arg]),
     S#{acc0 => Acc0
       ,loc => Loc
+      ,call => Call
       };
 is_containing_rountines(_Expr, S) ->
     %% io:format("??? ~p\n", [_Expr]),
@@ -81,5 +85,33 @@ is_list_of_funs({cons,_, F={'fun',_, {function,_FName,1}}, Cons}, Fs) ->
 is_list_of_funs({nil,_}, Fs) -> Fs;
 is_list_of_funs(_, _) -> false.
 
+
+rm_match([], _) -> [];
+rm_match([Match | Rest], #{match := Match}) ->
+    Rest;
+rm_match([NotAMatch | Rest], S) ->
+    [NotAMatch | rm_match(Rest, S)].
+
+replace_call([], _) -> [];
+replace_call([Call | Rest]
+            ,#{call := Call
+              ,loc := Loc
+              ,acc0 := Acc0
+              ,routines := Routines
+              }) ->
+    NewCall = calls(Routines, Acc0, Loc),
+    [NewCall|Rest];
+replace_call([NotACall|Rest], S) ->
+    [NotACall | replace_call(Rest, S)].
+
+loc(Loc, Node) ->
+    setelement(2, Node, Loc).
+
+calls([], Acc0, Loc) -> loc(Loc, Acc0);
+calls([F|Routines], Acc0, Loc) ->
+    fun_to_call(F, Loc, calls(Routines, Acc0, Loc)).
+
+fun_to_call(F={'fun',_FLoc,{function,_F,_A}}, Loc, Arg) ->
+    {call,Loc, F, [Arg]}.
 
 %% End of Module.
