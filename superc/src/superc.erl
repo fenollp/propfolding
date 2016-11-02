a531ad2c9b19775746992d2f6042ef4231f73f7f
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-01 18:35:06 -0700

    superc: init code

---
+++ superc/src/superc.erl
@@ -0,0 +1,33 @@
+%% Copyright © 2016 Pierre Fenoll ‹pierrefenoll@gmail.com›
+%% See LICENSE for licensing information.
+%% -*- coding: utf-8 -*-
+-module(superc).
+
+%% superc: 
+
+-export([parse_transform/2]).
+
+
+%% API
+
+parse_transform(Forms, _Options) ->
+    %% parse_trans:plain_transform(fun pt/1, Forms).
+    case parse_trans:depth_first(
+           fun (Ty, Form, Ctx, Acc) ->
+                   io:format(">>> Ty:~p\n\tForm:~p\n\tCtx:~p\n\tAcc:~p\n", [Ty, Form, Ctx, Acc]),
+                   {Form, Acc}
+           end, [], Forms, _Options
+          )
+    of
+        {error, Es} -> Es ++ Forms;
+        {NewForms, _} ->
+            parse_trans:revert(NewForms)
+    end.
+
+%% Internals
+
+pt(_Node) ->
+    io:format(">>> ~p\n", [_Node]),
+    continue.
+
+%% End of Module.
---
+++ superc/test/unfold_tests_c.erl
@@ -0,0 +1,23 @@
+%% Copyright © 2016 Pierre Fenoll ‹pierrefenoll@gmail.com›
+%% See LICENSE for licensing information.
+%% -*- coding: utf-8 -*-
+-module(unfold_tests_c).
+
+%% unfold_tests: tests for module unfold.
+
+-export([t/0]).
+-record(r, {'1', '2', '3', '4', '5'}).
+
+%% API tests.
+
+t() ->
+    Fs = [fun (R) -> setelement(1+2, R, "deux") end
+         ,fun (R) -> setelement(1+3, R, "trois") end
+         ,fun (R) -> setelement(1+5, R, "cinq") end
+         ,fun (R) -> setelement(1+2, R, "DEUX") end
+         ],
+    lists:foldl(fun (F, R) -> F(R) end, #r{}, Fs).
+
+%% Internals
+
+%% End of Module.
